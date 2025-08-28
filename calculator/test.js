9299259118c7c0cd2cb65098038b964705365454
leytonly <leytonlyy@gmail.com>
2025-08-28 14:33:40 +0200

    init husky

--- calculator/test.js
+++ calculator/test.js
@@ -1,15 +1,15 @@
 // importer le fichier operations.js
-const operations = require('./operations.js');
+const operations = require("./operations.js");
 // importer la librairie assert
-const assert = require('assert');
+const assert = require("assert");
 
-describe('testing', () => {
-	// tester chaque fonction de operation.js
-	it('should be 40 when you add 20 and 20', () => {
-	    // test ...
-          const wantedResult = 40; 
-          const result = operations.add(20,20);
-          // order matters given result, wanted result
-          assert.equal(result, wantedResult);
-      })
-})
+describe("testing", () => {
+  // tester chaque fonction de operation.js
+  it("should be 40 when you add 20 and 20", () => {
+    // test ...
+    const wantedResult = 40;
+    const result = operations.add(20, 20);
+    // order matters given result, wanted result
+    assert.equal(result, wantedResult);
+  });
+});
